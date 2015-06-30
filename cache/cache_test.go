5e91b045ae156eccb588ed36768b881789845d2d
akanksha gupta <akanksha.gupta@goibibo.com>
2015-06-30 19:03:29 +0530

    "Adding test cases"

---
+++ cache/cache_test.go
@@ -0,0 +1,50 @@
+package cache
+ 
+import (
+    "testing"
+    "cache"
+    "github.com/stretchr/testify/assert"
+    "fmt"
+)
+												
+ 
+func Test_SetData(t *testing.T) {
+	c:= cache.New("goleveldb",false)
+
+	for i := 0; i < 1000; i++ {
+		key_str := fmt.Sprint("key",i)
+		val_str := fmt.Sprint("value",i)
+		err := c.SetData(key_str,val_str,1000000)
+		assert.Nil(t, err, "Hope we don't have a error")
+		
+	}
+	c.CloseConnection()	
+}
+
+
+func Test_GetData(t *testing.T) {
+	c:= cache.New("goleveldb",false)
+	x, err := c.GetData("key999")
+	assert.Nil(t, err, "no error")
+	assert.Equal(t,"value999",x)
+	c.CloseConnection()	
+}
+
+func Test_DeleteData(t *testing.T) {
+	c:= cache.New("goleveldb",false)
+	err := c.DeleteData("key999")
+	assert.Nil(t, err, "no error")
+	c.CloseConnection()	
+}
+
+func Test_UpdateTTL(t *testing.T) {
+	c:= cache.New("goleveldb",false)
+	_, err := c.UpdateTTL("key999",20000)
+	assert.Nil(t, err, "no error")
+	c.CloseConnection()	
+
+}
+
+
+
+
